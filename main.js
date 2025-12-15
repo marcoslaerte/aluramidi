0c09f7f5c0a6185a41596b225b6c76ecb6965a6f
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2025-12-15 15:39:19 -0300

    Formas de executar uma função javascript ao clicar em um botão

--- main.js
+++ main.js
@@ -2,6 +2,24 @@ function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom');
+// Função tocaSomPom sendo guardada dentro de onclick do botão com a classe tecla_pom para ser executada quando o usuário clicar.
+document.querySelector('.tecla_pom').onclick = tocaSomPom; // Atribuicao da função ao onclick como se ele fosse um atributo
 
 
+// Forma 2
+// document.querySelector(".tecla_pom").onclick = function() {
+//     document.querySelector("#som_tecla_pom").play();
+// };
+
+// Forma 3
+// function tocaSomPom() {
+//     document.querySelector('#som_tecla_pom').play();
+// }
+// const botaoPom = document.querySelector('.tecla_pom');
+// botaoPom.addEventListener('click', tocaSomPom);
+
+// Forma 4
+// function tocaSomPom() {
+//     document.querySelector('#som_tecla_pom').play();
+// }
+// document.querySelector('.tecla_pom').addEventListener('click', tocaSomPom);
