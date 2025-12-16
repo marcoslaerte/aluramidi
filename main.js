38a0addc13fa9845816e641f88a759c41b93d701
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2025-12-16 19:57:15 -0300

    Simplificando os condicionais de verificação

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 function tocaSom(idElementoAudio) {
     const elemento = document.querySelector(idElementoAudio);
-    if (elemento === null) {
-        alert("Elemento não encontrado!");
-    }
-    if (elemento != null) {
-        if (elemento.localName === "audio") {
-            elemento.play();
-        }
+    
+    if (elemento != null && elemento.localName === "audio") {
+        elemento.play();
+    } else {
+        alert("Elemento não encontrado ou seletor inválido!");
     }
 }
 
